017d1a9760eabdb3c95f3c5825613589cb660bdb
Vhanne CLiff <me.giovanne@gmail.com>
2026-02-06 17:49:45 +0800

    Version 1.0

---
+++ Data.js
@@ -0,0 +1,6 @@
+export const members=[{
+  id:'',
+  name:'',
+  address:'',
+  age:''
+}]
---
+++ Registration.js
@@ -0,0 +1,15 @@
+import {members} from './Data.js'
+
+function register(){
+  members.forEach((account)=>{
+    if(account.name === newMember){
+      console.log("{newMember} is already a member!")
+    }else{
+      id:'',
+      name:'',
+      address:'',
+      age:''
+    }]
+    }
+  })
+}
