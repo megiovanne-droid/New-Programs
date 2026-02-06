4d1ba1614158b40b9d74bde76c2b3e3b1f140479
Vhanne CLiff <me.giovanne@gmail.com>
2026-02-06 19:52:09 +0800

    Version 1.30

--- search.js
+++ search.js
@@ -2,18 +2,20 @@ import {datas} from './Data.js'
 
 const searchName = document.querySelector(".js-search-name");
 const searchButton = document.querySelector(".js-search-button")
-searchButton.addEventListener(("click"),()=>{
+
+searchButton.addEventListener("click",()=>{
   search();
 })
 
-window.addEventListener(("keydown"),(event)=>{
+window.addEventListener("keydown",(event)=>{
   if(event.key === "Enter"){
     search();
   }
 })
 
 function search(){
-  name = searchName.value;
+  const name = searchName.value;
+
   datas.forEach((search)=>{
     if(search.name === name){
       console.log(`${name} is a member!`);
